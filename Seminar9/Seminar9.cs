0e8d3e16f23539f3feefca89ad2f33bf6bb3f83b
Natalia <iamnataliyaa@gmail.com>
2022-05-20 23:04:07 +0300

    Seminar 9

---
+++ Seminar9/Seminar9.cs
@@ -0,0 +1,11 @@
+﻿// Задача 1
+NaturalNums(5);
+void NaturalNums(int n)
+{
+    if (n >= 1)
+    {
+        Console.Write(n + " ");
+        NaturalNums(n - 1);
+    }
+}
+// Задача 2 Метод возведения числа А, в степень В
